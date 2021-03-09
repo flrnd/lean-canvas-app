68e5193af04616f080524991f96d8e14f1d7a9db
Florian Rand <florian.rand@gmail.com>
2021-03-09 17:56:06 +0100

    Placeholder problem component

--- src/components/problem.tsx
+++ src/components/problem.tsx
@@ -1,3 +1,22 @@
-const Problem = () => <div>Problem</div>
+import { useContext } from 'react'
+import { CanvasContext } from './Store'
+
+const Problem = () => {
+  const { store, setStore } = useContext(CanvasContext)
+  const data = ['New', 'Data', 'added']
+  const newStore = store
+  newStore['problem'] = data
+  setStore(newStore)
+  return (
+    <div>
+      {store['problem'].map((item) => (
+        <input type="text" value={item} />
+      ))}
+
+      <a href="/">Done</a>
+      {}
+    </div>
+  )
+}
 
 export default Problem
