28e241da295000b91b05b86dffb278c5b81c10df
Florian Rand <florian.rand@gmail.com>
2021-03-09 21:13:55 +0100

    Add single edit component

---
+++ src/components/edit.tsx
@@ -0,0 +1,27 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { CanvasContext } from './Store'
+
+const Edit = (props) => {
+  const { store, setStore } = useContext(CanvasContext)
+  const { location } = props.location.state
+  const data = store[location]
+
+  const handleData = (values: string[]) => {
+    const newStore = store
+    newStore[location] = values
+    setStore(newStore)
+  }
+
+  return (
+    <div>
+      {data.map((item) => (
+        <input type="text" value={item} />
+      ))}
+
+      <Link to="/">Done</Link>
+    </div>
+  )
+}
+
+export default Edit
