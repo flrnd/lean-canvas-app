24778d20ce759898f11ccd22543ff714aada6a07
Florian Rand <florian.rand@gmail.com>
2021-03-09 21:14:33 +0100

    Edit routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useRoutes } from 'hookrouter'
-import Routes from './router'
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
+import Canvas from './components/canvas'
+import Edit from './components/edit'
 
-function App() {
-  const routeResult = useRoutes(Routes)
-  return routeResult
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Canvas} />
+          <Route path="/edit" exact component={Edit} />
+        </Switch>
+      </Router>
+    </div>
+  )
 }
-
 export default App
