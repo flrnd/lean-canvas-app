1df19f779e14bfb3de4ea1e8dedd731923501782
Florian Rand <florian.rand@gmail.com>
2021-03-09 14:12:20 +0100

    Render Canvas rows with component

---
+++ src/components/canvas-row.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './canvas-row.css'
+
+interface Props {
+  name: string
+  linkTo: string
+}
+
+const CanvasRow = ({ name, linkTo }: Props) => {
+  return (
+    <a className={`${name} canvas`} href={linkTo}>
+      <div className="">
+        <div className="title">{name.replace(/-/g, ' ')}</div>
+      </div>
+    </a>
+  )
+}
+
+export default CanvasRow
--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,38 +1,25 @@
 import React from 'react'
+import CanvasRow from './canvas-row'
 import './canvas.css'
+
+const rows = [
+  'problem',
+  'solution',
+  'key-metrics',
+  'unique-value-prop',
+  'unfair-advantages',
+  'channels',
+  'customer-segments',
+  'cost-structure',
+  'revenue-streams',
+]
 
 const Canvas = () => {
   return (
     <div className="grid-container">
-      <a className="problem canvas" href="/problem">
-        <div className="">
-          <div className="title">Problem</div>
-        </div>
-      </a>
-      <div className="solution canvas">
-        <div className="title">Solution</div>
-      </div>
-      <div className="key-metrics canvas">
-        <div className="title">Key Metrics</div>
-      </div>
-      <div className="unique-value-prop canvas">
-        <div className="title">Unique Value Proposition</div>
-      </div>
-      <div className="unfair-advantages canvas">
-        <div className="title">Unfair Advantages</div>
-      </div>
-      <div className="channels canvas">
-        <div className="title">Channels</div>
-      </div>
-      <div className="customer-segments canvas">
-        <div className="title">Customer Segments</div>
-      </div>
-      <div className="cost-structure canvas">
-        <div className="title">Cost structure</div>
-      </div>
-      <div className="revenue-streams canvas">
-        <div className="title">Revenue Streams</div>
-      </div>
+      {rows.map((row) => (
+        <CanvasRow name={row} linkTo={`/${row}`} />
+      ))}
     </div>
   )
 }
