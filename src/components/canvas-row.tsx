09a4067d1b518dd8bc31b572e06c821cd601410c
Florian Rand <florian.rand@gmail.com>
2021-03-09 21:15:10 +0100

    Use Link from react route

--- src/components/canvas-row.tsx
+++ src/components/canvas-row.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './canvas-row.css'
 
 interface Props {
@@ -8,16 +9,21 @@ interface Props {
 
 const CanvasRow = ({ name, linkTo, data }: Props) => {
   return (
-    <a className={`${name} canvas`} href={linkTo}>
-      <div className="title">{name.replace(/-/g, ' ')}</div>
-      <div className="list">
-        <ul>
-          {data.map((listItem) => (
-            <li key={listItem}>{listItem}</li>
-          ))}
-        </ul>
+    <Link
+      className={`${name} canvas`}
+      to={{ pathname: '/edit', state: { location: linkTo } }}
+    >
+      <div>
+        <div className="title">{name.replace(/-/g, ' ')}</div>
+        <div className="list">
+          <ul>
+            {data.map((listItem) => (
+              <li key={listItem}>{listItem}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </a>
+    </Link>
   )
 }
 
