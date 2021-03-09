c27b02604a94aeba236cf09b9bdec996e434d9e5
Florian Rand <florian.rand@gmail.com>
2021-03-09 13:05:11 +0100

    Use hookrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,12 @@
 import React from 'react'
+import { useRoutes } from 'hookrouter'
+import Routes from './router'
+
 import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <div className="grid-container">
-        <div className="problem canvas">
-          <div className="title">Problem</div>
-        </div>
-        <div className="solution canvas">
-          <div className="title">Solution</div>
-        </div>
-        <div className="key-metrics canvas">
-          <div className="title">Key Metrics</div>
-        </div>
-        <div className="unique-value-prop canvas">
-          <div className="title">Unique Value Proposition</div>
-        </div>
-        <div className="unfair-advantages canvas">
-          <div className="title">Unfair Advantages</div>
-        </div>
-        <div className="channels canvas">
-          <div className="title">Channels</div>
-        </div>
-        <div className="customer-segments canvas">
-          <div className="title">Customer Segments</div>
-        </div>
-        <div className="cost-structure canvas">
-          <div className="title">Cost structure</div>
-        </div>
-        <div className="revenue-streams canvas">
-          <div className="title">Revenue Streams</div>
-        </div>
-      </div>
-    </div>
-  )
+  const routeResult = useRoutes(Routes)
+  return routeResult
 }
 
 export default App
