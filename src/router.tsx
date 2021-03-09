ed881be2b1c444726bdac3a0c00dba29976b16d2
Florian Rand <florian.rand@gmail.com>
2021-03-09 14:20:17 +0100

    Add rows routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Canvas from './components/canvas'
+import Channels from './components/channels'
+import CostStructure from './components/cost-structure'
+import CustomerSegments from './components/customer-segments'
+import KeyMetrics from './components/key-metrics'
 import Problem from './components/problem'
+import RevenueStreams from './components/revenue-streams'
+import UnfairAdvantages from './components/unfair-advantages'
+import UniqueValueProp from './components/unique-value-prop'
 
 const routes = {
   '/': () => <Canvas />,
   '/problem': () => <Problem />,
+  '/key-metrics': () => <KeyMetrics />,
+  '/unique-value-prop': () => <UniqueValueProp />,
+  '/unfair-advantages': () => <UnfairAdvantages />,
+  '/channels': () => <Channels />,
+  '/customer-segments': () => <CustomerSegments />,
+  '/cost-structure': () => <CostStructure />,
+  '/revenue-streams': () => <RevenueStreams />,
 }
 
 export default routes
