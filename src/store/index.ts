b35af50f6051cb0b5ba5cffc07957c3007987373
Florian Rand <florian.rand@gmail.com>
2021-03-15 16:04:16 +0100

    local stores object

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,29 +1,5 @@
 import { canvasData, rows, parseRowName } from './canvas-data'
 import CanvasContext from './canvas-context'
-import {
-  problemStore,
-  solutionStore,
-  keyMetricStore,
-  uniqueValuePropStore,
-  unfairAdvantagesStore,
-  channelsStore,
-  customerSegmentsStore,
-  costStructureStore,
-  revenueStreamsStore,
-} from './localCanvasStores'
+import { localCanvasDB } from './localCanvasStores'
 
-export {
-  CanvasContext,
-  canvasData,
-  rows,
-  parseRowName,
-  problemStore,
-  solutionStore,
-  keyMetricStore,
-  uniqueValuePropStore,
-  unfairAdvantagesStore,
-  channelsStore,
-  customerSegmentsStore,
-  costStructureStore,
-  revenueStreamsStore,
-}
+export { CanvasContext, canvasData, rows, parseRowName, localCanvasDB }
--- src/store/localCanvasStores.tsx
+++ src/store/localCanvasStores.tsx
@@ -1,69 +1,57 @@
 import localforage from 'localforage'
 import { rows } from '../store'
-const localCanvasDB = 'leanCanvasDB'
 
-const problemStore = localforage.createInstance({
-  name: localCanvasDB,
-  storeName: rows[0],
-  description: 'Problem row',
-})
+const dbName = 'localCanvasDB'
 
-const solutionStore = localforage.createInstance({
-  name: localCanvasDB,
-  storeName: 'solutionStore',
-  description: 'Solution row',
-})
-
-const keyMetricStore = localforage.createInstance({
-  name: localCanvasDB,
-  storeName: 'keyMetricStore',
-  description: 'Key Metrics row',
-})
-
-const uniqueValuePropStore = localforage.createInstance({
-  name: localCanvasDB,
-  storeName: 'uniqueValuePropStore',
-  description: 'Unique Value Props row',
-})
-
-const unfairAdvantagesStore = localforage.createInstance({
-  name: localCanvasDB,
-  storeName: 'unfairAdvantagesStore',
-  description: 'Unfair Advantages row',
-})
-
-const channelsStore = localforage.createInstance({
-  name: localCanvasDB,
-  storeName: 'channelsStore',
-  description: 'Channels row',
-})
-
-const customerSegmentsStore = localforage.createInstance({
-  name: localCanvasDB,
-  storeName: 'customerSegmentsStore',
-  description: 'Customer Segments row',
-})
-
-const costStructureStore = localforage.createInstance({
-  name: localCanvasDB,
-  storeName: 'costStructureStore',
-  description: 'Cost structure row',
-})
-
-const revenueStreamsStore = localforage.createInstance({
-  name: localCanvasDB,
-  storeName: 'revenueStremasStore',
-  description: 'Revenue streams row',
-})
-
-export {
-  problemStore,
-  solutionStore,
-  keyMetricStore,
-  uniqueValuePropStore,
-  unfairAdvantagesStore,
-  channelsStore,
-  customerSegmentsStore,
-  costStructureStore,
-  revenueStreamsStore,
+const localCanvasDB = {
+  name: dbName,
+  stores: {
+    problem: localforage.createInstance({
+      name: dbName,
+      storeName: rows[0],
+      description: 'Problem row',
+    }),
+    solution: localforage.createInstance({
+      name: dbName,
+      storeName: 'solutionStore',
+      description: 'Solution row',
+    }),
+    keyMetrics: localforage.createInstance({
+      name: dbName,
+      storeName: 'keyMetricStore',
+      description: 'Key Metrics row',
+    }),
+    uniqueValueProp: localforage.createInstance({
+      name: dbName,
+      storeName: 'uniqueValuePropStore',
+      description: 'Unique Value Props row',
+    }),
+    unfairAdvantages: localforage.createInstance({
+      name: dbName,
+      storeName: 'unfairAdvantagesStore',
+      description: 'Unfair Advantages row',
+    }),
+    channels: localforage.createInstance({
+      name: dbName,
+      storeName: 'channelsStore',
+      description: 'Channels row',
+    }),
+    customerSegments: localforage.createInstance({
+      name: dbName,
+      storeName: 'customerSegmentsStore',
+      description: 'Customer Segments row',
+    }),
+    costStructure: localforage.createInstance({
+      name: dbName,
+      storeName: 'costStructureStore',
+      description: 'Cost structure row',
+    }),
+    revenueStreams: localforage.createInstance({
+      name: dbName,
+      storeName: 'revenueStremasStore',
+      description: 'Revenue streams row',
+    }),
+  },
 }
+
+export { localCanvasDB }
